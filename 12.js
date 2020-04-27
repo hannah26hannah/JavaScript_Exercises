79b1a67b378933877abf455d7f2910b988d5660f
hannah26hannah <ooyoonoo24@gmail.com>
2020-04-27 21:52:14 +0900

    document.URL(), document.location.href()
     Write a JavaScript program to get the website URL (loading page).

---
+++ 12.js
@@ -0,0 +1,9 @@
+// protocol + domain + page
+// document.URL "http://example.com/page1.html"
+// this URL read-only property of the Document interface returns the document location as a string.
+
+// console.log(document.URL)
+// document.location.href "http://example.com/page1.html"
+
+const url = document.location.href;
+console.log(url);
